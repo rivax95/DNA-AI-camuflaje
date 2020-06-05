c65b92a158cdcf656506b9b3c6f7f5cae6f22cb5
Alex <alejandrotejemundos@hotmail.es>
2020-06-05 16:35:00 +0200

    Up GitHub

---
+++ Assets/ADN.cs
@@ -0,0 +1,39 @@
+﻿//                                          ▂ ▃ ▅ ▆ █ arc █ ▆ ▅ ▃ ▂ 
+//                                        ..........<(+_+)>...........
+// ADN.cs (22/12/18)
+//Autor: Alejandro Rivas                 alejandrotejemundos@hotmail.es
+//Desc:
+//Mod : 
+//Rev :
+//..............................................................................................\\
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADN : MonoBehaviour {
+
+    //Gen del color
+    public float r;
+    public float g;
+    public float b;
+    public float s;
+    bool dead = false;
+    public float timeToDie;
+    SpriteRenderer srender;
+    Collider2D sCollider;
+    void Start()
+    {
+        srender = GetComponent<SpriteRenderer>();
+        sCollider = GetComponent<Collider2D>();
+        srender.color = new Color(r, g, b);
+        transform.localScale = new Vector3(0.267065f, s, 0.267065f);
+
+    }
+    void OnMouseDown()
+    {
+        dead = true;
+        timeToDie = PopulationManager.elapsed;
+        srender.enabled = false;
+        sCollider.enabled = false;
+    }
+}
---
+++ Assets/PopulationManager.cs
@@ -0,0 +1,89 @@
+﻿//                                          ▂ ▃ ▅ ▆ █ ZEN █ ▆ ▅ ▃ ▂ 
+//                                        ..........<(+_+)>...........
+// .cs (//)
+//Autor: Alejandro Rivas                 alejandrotejemundos@hotmail.es
+//Desc:
+//Mod : 
+//Rev :
+//..............................................................................................\\
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+public class PopulationManager : MonoBehaviour {
+    public GameObject personPrefab;
+
+    public int PopulationSize = 10;
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    public int trialtimee = 10;
+    int generation = 1;
+
+    public Text gen;
+    public Text trialtime;
+	void Start () {
+        for (int i = 0; i < PopulationSize; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0);
+            GameObject go = Instantiate(personPrefab, pos, Quaternion.identity);
+            go.GetComponent<ADN>().r = Random.Range(0.0f, 1.0f);
+            go.GetComponent<ADN>().g = Random.Range(0.0f, 1.0f);
+            go.GetComponent<ADN>().b = Random.Range(0.0f, 1.0f);
+            go.GetComponent<ADN>().s = Random.Range(0.1f, 0.4f);
+            population.Add(go);
+        }
+	}
+	
+	
+	void Update () {
+        gen.text = "Gen " + generation;
+        trialtime.text = "Time: " + trialtimee;
+        elapsed += Time.deltaTime;
+        if (elapsed > trialtimee)
+        {
+            BreedNewPopulation();
+            elapsed = 0;
+        }
+	}
+    GameObject Breed(GameObject parent1, GameObject parent2)
+    {
+        Vector3 pos = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f), 0);
+        GameObject go = Instantiate(personPrefab, pos, Quaternion.identity);
+        ADN adn1 = parent1.GetComponent<ADN>();
+        ADN adn2 = parent2.GetComponent<ADN>();
+        if (Random.Range(0, 10) < 9)
+        {
+            go.GetComponent<ADN>().r = Random.Range(0, 10) < 5 ? adn1.r : adn2.r;
+            go.GetComponent<ADN>().g = Random.Range(0, 10) < 5 ? adn1.g : adn2.g;
+            go.GetComponent<ADN>().b = Random.Range(0, 10) < 5 ? adn1.b : adn2.b;
+            go.GetComponent<ADN>().s = Random.Range(0, 10) < 5 ? adn1.s : adn2.s;
+        }
+        else
+        {
+            go.GetComponent<ADN>().r = Random.Range(0.0f, 1.0f);
+            go.GetComponent<ADN>().g = Random.Range(0.0f, 1.0f);
+            go.GetComponent<ADN>().b = Random.Range(0.0f, 1.0f);
+            go.GetComponent<ADN>().s = Random.Range(0.1f, 0.4f);
+
+        }
+        return go;
+    }
+    void BreedNewPopulation()
+    {
+        List<GameObject> newpopulation= new List<GameObject>();
+        List<GameObject> sortedList = population.OrderByDescending(o => o.GetComponent<ADN>().timeToDie).ToList();
+        population.Clear();
+        for (int i = (int) (sortedList.Count/2.0f)-1; i < sortedList.Count-1; i++)
+        {
+            population.Add(Breed(sortedList[i], sortedList[i + 1]));
+            population.Add(Breed(sortedList[i + 1], sortedList[i]));
+        }
+        for (int i = 0; i < sortedList.Count; i++)
+        {
+            Destroy(sortedList[i]);
+        }
+        generation++;
+
+    }
+}
